87381b9d2d06e2c1fe0d171065e594535be4640e
Minung Han <hmu332233@gmail.com>
2023-09-04 22:34:47 +0900

    vite app에 nextui 추가

--- packages/app2/src/App.tsx
+++ packages/app2/src/App.tsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Button } from '@nextui-org/react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   useEffect(() => {
     // const words = {
     //   extensions:
@@ -13,37 +10,17 @@ function App() {
     //   popup:
     //     "A UI surface which appears when an extension's action icon is clicked."
     // };
-
-    chrome.runtime.onMessage.addListener(({ name, data }) => {
-      if (name === 'define-word') {
-        console.log(name, data);
-      }
-    });
+    // chrome.runtime.onMessage.addListener(({ name, data }) => {
+    //   if (name === 'define-word') {
+    //     console.log(name, data);
+    //   }
+    // });
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button color="primary">테스트</Button>
+    </div>
   );
 }
 

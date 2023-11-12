fa052567f7ee63cc54801f3a1445e9d1768b9fd6
Minung Han <hmu332233@gmail.com>
2023-11-12 22:30:42 +0900

    extension context 코드 추가

--- packages/chrome/service-worker.js
+++ packages/chrome/service-worker.js
@@ -1,8 +1,8 @@
 function setupContextMenu() {
   chrome.contextMenus.create({
-    id: 'define-word',
-    title: 'Define',
-    contexts: ['selection'],
+    id: 'syncExp',
+    title: 'Sync Exp',
+    contexts: ['editable'],
   });
 }
 
@@ -10,9 +10,49 @@ chrome.runtime.onInstalled.addListener(() => {
   setupContextMenu();
 });
 
-chrome.contextMenus.onClicked.addListener((data) => {
-  chrome.runtime.sendMessage({
-    name: 'define-word',
-    data: { value: data.selectionText },
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: insertExpLink,
   });
 });
+
+function insertExpLink() {
+  const pageTitle = document.title;
+  const pageUrl = window.location.href;
+  const activeElement = document.activeElement;
+
+  // 현재 활성화된 요소가 contenteditable 요소인지 확인합니다.
+  if (activeElement.isContentEditable) {
+    // URL과 데이터 정의
+    const url = 'http://localhost:3000/api/campaigns';
+    const data = {
+      title: pageTitle,
+      url: pageUrl,
+    };
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then(({ campaignId }) => {
+        const selection = window.getSelection();
+        const range =
+          selection.rangeCount > 0
+            ? selection.getRangeAt(0)
+            : document.createRange();
+
+        // 삽입할 텍스트를 생성합니다.
+        const textNode = document.createTextNode(campaignId);
+        range.deleteContents();
+        range.insertNode(textNode);
+
+        // 삽입 후 새로운 커서 위치를 설정합니다.
+        range.setStartAfter(textNode);
+        range.setEndAfter(textNode);
+        selection.removeAllRanges();
+        selection.addRange(range);
+      });
+  }
+}
